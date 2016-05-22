62a34bd43360d9eecd0a44dd14ccb8b24ec440e0
vandreev <vandreevwork94@gmail.com>
2016-05-22 21:51:06 +0300

    run command 'npm run gulp' for build css and js into 'public' dir

---
+++ Gulpfile.js
@@ -0,0 +1,45 @@
+
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var webpack = require('webpack-stream');
+
+gulp.task('default', ['style', 'js']);
+
+gulp.task('js', function(){
+return gulp.src('./js/app.js')
+  .pipe(webpack({
+    output: {
+        filename: 'main.js',
+     },
+     module: {
+      loaders: [{
+       test: /\.js$/,
+       exclude: /(node_modules)/,
+       loader: 'babel',
+       query: {
+        presets: ['es2015']
+       }
+      }],
+     },
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('style', function() {
+    var processors = [
+        autoprefixer({browsers: ['last 2 version']}),
+        cssnano(),
+    ];
+    return gulp.src('./style/*.scss')
+     .pipe(sass().on('error', sass.logError))
+     .pipe(postcss(processors))
+     .pipe(concat('main.css'))
+     .pipe(gulp.dest('./public'));  
+});
