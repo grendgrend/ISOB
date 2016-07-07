afc24da9666fa2d67df042611b481157760c08f7
Kseniya Novinskaya <k.l.novinskaya@gmail.com>
2016-07-07 19:10:15 +0300

    Добавлены комментарии к методам

--- server/AbsDatabase.php
+++ server/AbsDatabase.php
@@ -4,14 +4,14 @@ abstract class absDatabase {
     protected $userName;
     protected $password;
     protected $database;
-    function __construct($host, $user, $pas, $DB){
+    function __construct($host, $user, $pas, $DB){//Принимает имя хоста, пользователя, пароль и имя БД. Инициализирует соединение с БД
         $this->host=$host;
         $this->userName=$user;
         $this->password=$pas;
         $this->database=$DB;
         $this->connectToDB();
     }
-    private function connectToDB (){
+    private function connectToDB (){ // Метод, который устанавливает соединение с БД, параметры которой задаются в функции-конструкторе
         $this->mysqli = new mysqli($this->host, $this->userName, $this->password, $this->database);
         $this->mysqli->set_charset("utf8");
     }
--- server/GalleryDatabase.php
+++ server/GalleryDatabase.php
@@ -4,13 +4,13 @@ class galleryDatabase extends absDatabase{
     function __construct($host, $user, $pas, $DB) {
         parent::__construct($host, $user, $pas, $DB);
     }
-    private function getGalleryItems (){
+    private function getGalleryItems (){//Отправляет базе запрос на получение всех картин галереи
         $this->resultItems = $this->mysqli->query("SELECT * FROM Items");
     }
-    private function getGalleryTags (){
+    private function getGalleryTags (){// Отправляет запрос на получение всех поисковых тегов галереи
         $this->resultTags = $this->mysqli->query("SELECT * FROM tags");
     }
-    public function returnGalleryItems (){
+    public function returnGalleryItems (){// Метод формирует массив значений для дальнейшей отрисовки. Он сам обращается к методу, который посылает запрос к БД. Возващает массив картин и их свойств
         $this->getGalleryItems();
         $items=array();
         foreach ($this->resultItems as $value){
@@ -23,7 +23,7 @@ class galleryDatabase extends absDatabase{
         shuffle($items);
         return $items;
     }
-    public function returnGalleryTags () {
+    public function returnGalleryTags () {// Метод формирует и отдает массив тегоы для отрисовки. Он сам обращается к методу, который посылает запрос к БД.
         $this->getGalleryTags();
         $tags=array();
         foreach ($this->resultTags as $value){
