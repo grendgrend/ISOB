cf8827dd180e0306ae329dac931fc5b637b29795
Kseniya Novinskaya <k.l.novinskaya@gmail.com>
2016-07-07 19:24:24 +0300

    Поправлены методы, убрано дублирование

--- server/GalleryDatabase.php
+++ server/GalleryDatabase.php
@@ -4,16 +4,13 @@ class galleryDatabase extends absDatabase{
     function __construct($host, $user, $pas, $DB) {
         parent::__construct($host, $user, $pas, $DB);
     }
-    private function getGalleryItems (){//Отправляет базе запрос на получение всех картин галереи
-        $this->resultItems = $this->mysqli->query("SELECT * FROM Items");
-    }
-    private function getGalleryTags (){// Отправляет запрос на получение всех поисковых тегов галереи
-        $this->resultTags = $this->mysqli->query("SELECT * FROM tags");
+    private function select ($item, $table){// Отправляет запрос на получение $item из $table
+        $this->result = $this->mysqli->query("SELECT $item FROM $table");
     }
     public function returnGalleryItems (){// Метод формирует массив значений для дальнейшей отрисовки. Он сам обращается к методу, который посылает запрос к БД. Возващает массив картин и их свойств
-        $this->getGalleryItems();
+        $this->select('*','Items');
         $items=array();
-        foreach ($this->resultItems as $value){
+        foreach ($this->result as $value){
             $items[]=array(
                 'URL'=>$value['url'],
                 'name'=>$value['name'],
@@ -23,10 +20,10 @@ class galleryDatabase extends absDatabase{
         shuffle($items);
         return $items;
     }
-    public function returnGalleryTags () {// Метод формирует и отдает массив тегоы для отрисовки. Он сам обращается к методу, который посылает запрос к БД.
-        $this->getGalleryTags();
+    public function returnGalleryTags () {// Метод формирует и отдает массив тегов для отрисовки. Он сам обращается к методу, который посылает запрос к БД.
+        $this->select('*','tags');
         $tags=array();
-        foreach ($this->resultTags as $value){
+        foreach ($this->result as $value){
             $tags[]=array(
                 'name'=>$value['tag']
             );
